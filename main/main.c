164012f579cea40974802f0a94d638b86bb9947e
João Carlos <doidobr46@gmail.com>
2023-10-21 17:10:16 -0300

    Trains moving and working as expected.

--- main/main.c
+++ main/main.c
@@ -8,14 +8,19 @@
 #include "types.h"
 #include "print.h"
 
-#define PRINT_DELAY 500
+#define PRINT_DELAY 100
+#define TRAIN_STOP_IN_STATION_DELAY 2000
+#define TRAIN_MOVE_DELAY 200
 
-static const char *TAG = "file_system";
+static const char *FILE_SYSTEM_TAG = "file_system";
+static const char *MAIN_TAG = "railroad_system";
 
 station_t stations[NUM_STATIONS];
 train_t trains[NUM_TRAINS];
 
-int findPositionInRailway(const position_t *railway, size_t num_points, const position_t *target) {
+SemaphoreHandle_t trains_mutex, stations_mutex;
+
+int8_t findPositionInRailway(const position_t *railway, size_t num_points, const position_t *target) {
     for (int i = 0; i < num_points; i++) {
         if (railway[i].x == target->x && railway[i].y == target->y) {
             return i;
@@ -27,30 +32,76 @@ int findPositionInRailway(const position_t *railway, size_t num_points, const po
 void init_stations(){
     for (uint8_t i = 0; i < NUM_STATIONS; i++) {
         stations[i].station_pos = STATIONS_POSITIONS[i];
+        stations[i].station_index = findPositionInRailway(RAILWAY,RAILWAY_SIZE,&stations[i].station_pos);
+        if(-1 == stations[i].station_index){
+            ESP_LOGE(MAIN_TAG, "Station position not found in the railway");
+            return;
+        }
         stations[i].station_id = 65 + i;
+        stations[i].is_train_parked = false;
     }
-    ESP_LOGI(TAG, "Stations successfully initiated.");
+    ESP_LOGI(MAIN_TAG, "Stations successfully initiated.");
 }
 
 void init_trains(){
-    ESP_LOGI(TAG, "Trains successfully initated.");
     for (uint8_t i = 0; i < NUM_TRAINS; i++) {
         //In conio_linux.h the first color is black, which is the same as the terminal background
         //So we start from the first color on.
         trains[i].train_color = 1 + i;
         trains[i].current_pos = STATIONS_POSITIONS[i];  //Trains start in different stations.
-        trains[i].train_number = 1 + i ;
+        trains[i].train_number = 1 + i;
         trains[i].current_index = findPositionInRailway(RAILWAY,RAILWAY_SIZE, &trains[i].current_pos);
+        if(-1 == trains[i].current_index){
+            ESP_LOGE(MAIN_TAG, "Train position not found in the railway");
+            return;
+        }
+    }
+    ESP_LOGI(MAIN_TAG, "Trains successfully initated.");
+}
+
+void train_task(void *params){
+    uint8_t *p_train_number = (uint8_t *)params;
+    uint8_t train_number = *p_train_number;
+    bool stopped_in_station = false;
+    while (true)
+    {
+        if(trains[train_number].current_index < RAILWAY_SIZE){
+            xSemaphoreTake(stations_mutex, portMAX_DELAY);
+            stations[trains[train_number].current_index].is_train_parked = false;
+            xSemaphoreGive(stations_mutex);
+            xSemaphoreTake(trains_mutex, portMAX_DELAY);
+            trains[train_number].current_index++;
+            trains[train_number].current_pos = RAILWAY[trains[train_number].current_index];
+            xSemaphoreGive(trains_mutex);
+        } else {
+            trains[train_number].current_index = 0;
+        }
+        stopped_in_station = false;
+        for (size_t i = 0; i < NUM_STATIONS; i++)
+        {
+            if(stations[i].station_index == trains[train_number].current_index){
+                stopped_in_station = true;
+                xSemaphoreTake(stations_mutex, portMAX_DELAY);
+                stations[i].is_train_parked = true;
+                xSemaphoreGive(stations_mutex);
+            }
+        }
+        if(stopped_in_station){
+            vTaskDelay(pdMS_TO_TICKS(TRAIN_STOP_IN_STATION_DELAY));
+        } else {
+            vTaskDelay(pdMS_TO_TICKS(TRAIN_MOVE_DELAY));
+        }
+        vTaskDelay(pdMS_TO_TICKS(TRAIN_MOVE_DELAY));
     }
 }
 
-void print_map_task(void *args){
+void print_map_task(void *params){
     size_t contentSize = 0;
     // Open the file for reading
     FILE *file = fopen("/files/map.txt", "r");
 
     if (file == NULL) {
-        ESP_LOGE(TAG,"Failed to open the file.\n");
+        ESP_LOGE(FILE_SYSTEM_TAG, "Failed to open the file.\n");
         return;
     }
     // Seek to the beginning of the file
@@ -68,17 +119,17 @@ void print_map_task(void *args){
     size_t bytesRead = fread(fileContent, 1, contentSize, file);
 
     if (bytesRead != contentSize) {
-        ESP_LOGE(TAG,"Failed to read the file.\n");
+        ESP_LOGE(FILE_SYSTEM_TAG, "Failed to read the file.\n");
         return;
     }
     fileContent[contentSize] = '\0';
 
     while (1) {
+        print_trains(trains);
         gotoxy(0,0);
         printf("%s\n", fileContent);
-        print_trains(trains);
-        gotoxy(0,22);
         vTaskDelay(pdMS_TO_TICKS(PRINT_DELAY));
+        gotoxy(0,22);
     }
 }
 
@@ -94,22 +145,42 @@ void app_main() {
     
     esp_err_t ret = esp_vfs_spiffs_register(&conf);
     if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        ESP_LOGE(FILE_SYSTEM_TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
         return;
     }
 
     size_t total = 0, used = 0;
     ret = esp_spiffs_info(conf.partition_label,&total,&used);
     if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to get partition info (%s)", esp_err_to_name(ret));
+        ESP_LOGE(FILE_SYSTEM_TAG, "Failed to get partition info (%s)", esp_err_to_name(ret));
         return;
     } else {
-        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+        ESP_LOGI(FILE_SYSTEM_TAG, "Partition size: total: %d, used: %d", total, used);
     }
     
+    trains_mutex = xSemaphoreCreateMutex();
+    if (trains_mutex == NULL) {
+        ESP_LOGE(MAIN_TAG, "Trains mutex creation failed");
+        return;
+    }
+    xSemaphoreGive(trains_mutex);
+
+    stations_mutex = xSemaphoreCreateMutex();
+    if (stations_mutex == NULL) {
+        ESP_LOGE(MAIN_TAG, "Stations mutex creation failed");
+        return;
+    }
+    xSemaphoreGive(stations_mutex);
+
     init_stations();
     init_trains();
     clrscr();
 
+
     xTaskCreate(print_map_task, "PrintMapTask", 4096, NULL, 3, NULL);
+
+    for (uint8_t i = 0; i < NUM_TRAINS; i++)
+    {
+        xTaskCreate(train_task, "TrainTask", 4096, &i, 2, NULL);
+    }
 }
--- main/types.h
+++ main/types.h
@@ -5,7 +5,7 @@
 
 #define NUM_TRAINS 4
 #define NUM_STATIONS 6
-#define RAILWAY_SIZE 54
+#define RAILWAY_SIZE 52
 
 typedef struct position_struct {
     uint8_t x;
@@ -14,12 +14,14 @@ typedef struct position_struct {
 
 typedef struct station_struct {
     position_t station_pos;
+    int8_t station_index;
     uint8_t station_id;
+    bool is_train_parked;
 } station_t;
 
 typedef struct train_struct {
     position_t current_pos;
-    uint8_t current_index;
+    int8_t current_index;
     uint8_t train_color;
     uint8_t train_number;
 } train_t;
@@ -29,16 +31,16 @@ const position_t RAILWAY[RAILWAY_SIZE] = {
     {3, 16}, {2, 17}, {1, 18}, {2, 19}, {3, 20}, {5, 21}, {6, 21}, {7, 21}, {8, 21}, {10, 20}, {11, 19},
     {12, 18}, {13, 17}, {14, 16}, {15, 15}, {16, 14}, {16, 13}, {16, 12}, {16, 11}, {17, 10}, {18, 9},
     {19, 8}, {20, 7}, {19, 6}, {18, 5}, {16, 4}, {14, 3}, {13, 2}, {11, 1}, {10, 1}, {9, 1}, {8, 1}, {7, 1},
-    {6, 1}, {5, 1}, {3, 2}, {2, 3}, {2, 3}, {2, 3}, {1, 4}, {1, 5}, {2, 6}, {3, 7}
+    {6, 1}, {5, 1}, {3, 2}, {2, 3}, {1, 4}, {1, 5}, {2, 6}, {3, 7}
 };
 
-const position_t STATIONS_POSITIONS[] = {
+const position_t STATIONS_POSITIONS[NUM_STATIONS] = {
     {6, 8},
     {5, 15},
     {7, 21},
     {16, 13},
-    {7, 20},
-    {1, 8}
+    {20, 7},
+    {7, 1}
 };
 
 #endif //_TYPES_H_
