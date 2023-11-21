ada6919ec71358a7c030351a2596e6849fa4fd1b
João Carlos <doidobr46@gmail.com>
2023-11-21 12:01:48 -0300

    Time to arrival almost complete.

--- main/utils.h
+++ main/utils.h
@@ -4,40 +4,25 @@
 #include <stdio.h>
 #include "types.h"
 
-uint8_t calculateTimeToStation(
-    uint8_t current_index, uint8_t target_index,
+uint8_t calculate_time_to_station(
+    uint8_t train_index, uint8_t station_index,
     uint8_t station_stop_time, uint8_t train_normal_speed_in_seconds,
     uint8_t train_slow_speed_in_seconds, int train_status
     ) {
     uint8_t total_time = 0;
-
-    for (int i = 0; i < RAILWAY_SIZE; ++i) {
-        int next_index = (current_index + 1) % RAILWAY_SIZE; // Circular traversal
-
-        // Check if the current index is the target station
-        if (current_index == target_index) {
-            break;
-        }
-
-        uint8_t distance = next_index - current_index;
-
-        if(train_status == SLOW_SPEED){
-            total_time += distance * train_normal_speed_in_seconds + train_slow_speed_in_seconds;
-        } else {
-            total_time += distance * train_normal_speed_in_seconds;
-        }
-
-        // Check if the current index is a station, add station stop time
-        for (int j = 0; j < NUM_STATIONS; ++j) {
-            if (current_index == target_index) {
-                total_time += station_stop_time;
-                break;
-            }
-        }
-
-        // Move to the next index
-        current_index = next_index;
+    
+    //Computes time between train and station based only on train tracks between them
+    if(station_index > train_index){
+        total_time = station_index - train_index;
+    } else {
+        total_time = RAILWAY_SIZE - (train_index - station_index);
     }
+    
+    //Adds slowed speed time contributions
+    if(train_status == SLOW_SPEED){
+        total_time += train_slow_speed_in_seconds;
+    }
+    
     return total_time;
 }
 
